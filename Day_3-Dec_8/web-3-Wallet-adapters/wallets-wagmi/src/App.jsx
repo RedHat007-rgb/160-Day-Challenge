1e26d3f5e2fc90d1cd20d4b811aa39c5882da069
RedHat007-rgb <cu.16bcs1288@gmail.com>
2024-12-11 16:17:43 +0530

    learned about tanstack library

--- Day_3-Dec_8/web-3-Wallet-adapters/wallets-wagmi/src/App.jsx
+++ Day_3-Dec_8/web-3-Wallet-adapters/wallets-wagmi/src/App.jsx
@@ -1,23 +1,79 @@
-import { useState ,useEffect} from 'react'
-import { QueryClient } from '@tanstack/react-query';
-import './App.css'
-const queryClient = new queryClient();
 
+import './App.css'
+import { QueryClient, QueryClientProvider, useQuery} from '@tanstack/react-query';
+const queryClient=new QueryClient();//global variable for all the fetches.
 
 async function getter(){
-  const data=await fetch("https://jsonplaceholder.typicode.com/posts");
-  const response=await data.json();
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const response = await data.json();
   return response;
 }
 
+
+
 function App() {
+  // const[posts,setPosts]=useState([]);
+  // // const[titles,setTitles]=useState([]);
+  // //const[error,setError]=useState();
+   
+  // async function getPosts(){
+  //   try{
+  //   const response=await fetch("https://jsnplaceholder.typicode.com/posts");
+  //   const json=await response.json();//here we can use axios library also to fetch the data.
+  //   setPosts(json); 
+  // } catch {
+  //   console.log("Url is not correct")
+  //   // setError("Url is not defined");
+  //   // console.log(error)
+  // }
+
+  // } 
+  
+  // useEffect(function (){
+  //  setInterval(function (){
+  //   getPosts();
+  //  },5000);
+  // },[])
+
+  // // useEffect(function(){//This can be used without map function to render a title in the browser.
+  // //   let newArray=[];
+  // //   for (let i=1;i<posts.length;i++){
+  // //     console.log(posts[i].title);
+  // //     newArray.push(posts[i].title);
+  // //     setTitles(newArray);
+  // // }
+  // // },[posts])
   
 
-  return (
-    <>
-     {posts.map(post=> <div key={post.id}>{post.title}</div>)}
-    </>
+  return (//here we are converting an object to a string using stringify
+    <QueryClientProvider client={queryClient}>
+      <Posts/>
+   </QueryClientProvider>
   )
 }
 
+function Posts(){
+ 
+  const {data,isloading,error} =useQuery({queryKey:['posts'],queryFn:getter});
+  
+  if(error){
+    return <div>
+      Error while fetching..
+    </div>
+  }
+  if(isloading){
+    return <div>
+     Loading.....
+    </div>
+  }
+  if(data){
+    return <div>
+    {data.map((item) => (<div key={item.id}>{item.title}</div>))} 
+    </div> 
+  }
+}
+  
+
+
 export default App
+
